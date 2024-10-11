39ebd04218a0df43cf3345507abebcb83dedd07d
marioguerra2002 <alu0101395036@ull.edu.es>
2024-10-11 16:34:30 +0100

    inicialización del proyecto

---
+++ src/matriz_lab.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "nodo.h"
+
+class MatrizLab {
+  public:
+
+  private:
+    std::vector<Nodo> matriz_nodos;
+
+};
---
+++ src/nodo.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Nodo {
+  public:
+    Nodo(int x, int y, int coste_estimado, int pared_num);
+    void set_coste(int coste) { this->coste = coste; }
+    void set_coste_acumulado(int coste_estimado_, int coste) { this->coste_acumulado = coste_estimado_ + coste; }
+    void set_pared(bool pared) { this->pared = pared; } // en base a true o false se pone el nodo como pared o no
+    void set_coste_estimado(int meta_x, int x, int meta_y, int y);
+    int get_x() { return x; }
+    int get_y() { return y; }
+    int get_coste() { return coste; }
+    int get_coste_estimado() { return coste_estimado; }
+    int get_coste_acumulado() { return coste_acumulado; }
+    bool get_pared() { return pared; }
+    bool operator < (const Nodo& nodo) const { return coste_acumulado < nodo.coste_acumulado; } // siempre cojo el más pequeño
+    
+
+
+  private:
+    int x; // Coordenada x
+    int y; // Coordenada y
+    int coste; // Coste de llegar a este nodo g(n)
+    int coste_estimado; // Coste estimado de llegar a la meta h(n)
+    bool pared = true; // Indica si el nodo es una pared
+    int coste_acumulado; // Coste acumulado de llegar a este nodo (fn)
+};
